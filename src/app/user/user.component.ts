6e2157cb257ac260247f981abba5fd33214cdf03
phpontes <ppontes95@hotmail.com>
2024-10-02 14:20:45 -0300

    Storing Data in a Component Class

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length)
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -8,5 +12,5 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  selectedUser = DUMMY_USERS[randomIndex];
 }
