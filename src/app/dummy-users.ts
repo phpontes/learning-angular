fdd38259beef4eb587dfaed94302196a8413596a
phpontes <ppontes95@hotmail.com>
2024-10-02 14:11:50 -0300

    Preparing User Data

---
+++ src/app/dummy-users.ts
@@ -0,0 +1,32 @@
+export const DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Jurema das Graças',
+    avatar: 'user-1.jpg',
+  },
+  {
+    id: 'u2',
+    name: 'Emília Pinto',
+    avatar: 'user-2.jpg',
+  },
+  {
+    id: 'u3',
+    name: 'Zebedias Horácio',
+    avatar: 'user-3.jpg',
+  },
+  {
+    id: 'u4',
+    name: 'Tonho das Ruas',
+    avatar: 'user-4.jpg',
+  },
+  {
+    id: 'u5',
+    name: 'Neiva Benedita',
+    avatar: 'user-5.jpg',
+  },
+  {
+    id: 'u6',
+    name: 'Orestes Mazola',
+    avatar: 'user-6.jpg',
+  },
+];
